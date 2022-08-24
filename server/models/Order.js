c0e791a6fc4ad07b09361f2d356a66a4d30e59ab
Pavel Lebedev <lebedpavel.dev@gmail.com>
2022-08-24 12:11:41 +0300

    fix: add config.js, change package.json & Order.js

--- server/models/Order.js
+++ server/models/Order.js
@@ -4,8 +4,8 @@ const Order = new mongoose.Schema({
     city: {type: String, required: true},
     route: {type: String, required: true},
     package: {type: String, required: true},
-    email: {type: String, required: true},
-    message: {type: String, required: true}
+    clientEmail: {type: String, required: true},
+    clientMessage: {type: String, required: true}
 });
 
 export default mongoose.model('Order', Order);
