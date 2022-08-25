e419e0a970bdf4e7f3c3b47972a0aca91ff260f6
Elena-code-dev <kev.76@inbox.ru>
2022-08-25 23:32:20 +0300

    feat: add slider

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
 import './index.scss';
 import './reset.css';
-//import bootstrap from 'bootstrap';
+
+const items = document.querySelectorAll('.list-group-item');
+const carousels = document.querySelectorAll('.carousel');
+
+for (let i = 0; i < items.length; i++) {
+  items[i].addEventListener('click', function () {
+    for (let j = 0; j < carousels.length; j++) {
+      if (i == j) {
+        carousels[j].classList.remove('inactive');
+      } else {
+        carousels[j].classList.add('inactive');
+      }
+    }
+  });
+}
