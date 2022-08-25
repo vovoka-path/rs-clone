109180456a86d6fcdb3f9104184de11c18339f50
Pavel Lebedev <lebedpavel.dev@gmail.com>
2022-08-25 19:21:02 +0300

    feat: expanded ordering scheme

--- server/models/Order.js
+++ server/models/Order.js
@@ -5,7 +5,27 @@ const Order = new mongoose.Schema({
     route: {type: String, required: true},
     package: {type: String, required: true},
     clientEmail: {type: String, required: true},
-    clientMessage: {type: String, required: true}
+    clientMessage: {type: String, required: true},
+    clientPhone: {type: String},
+    date: {
+        incomingOrder: {type: Date},
+        photographerAppointed: {type: Date},
+        photographerAccepted: {type: Date},
+        editorAppointed: {type: Date},
+        editorAccepted: {type: Date},
+        photoCompleted: {type: Date},
+        photoSended: {type: Date},
+    },
+    status: {type: String, default: 'incoming'},
+    photographerId: {type: String},
+    editorId: {type: String},
+    photographerLink: {type: String},
+    editorLink: {type: String},
+    comments: [{
+        from: {type: String},
+        to: {type: String},
+        message: {type: String}
+    }]
 });
 
 export default mongoose.model('Order', Order);
