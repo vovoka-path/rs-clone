452f2575cf906a7df3b968a4caf5949734ae23b4
Pavel Lebedev <lebedpavel.dev@gmail.com>
2022-08-30 15:15:02 +0300

    refactor: add props for user

--- server/models/User.js
+++ server/models/User.js
@@ -3,6 +3,8 @@ import mongoose from "mongoose";
 const User = new mongoose.Schema({
     username: {type: String, unique: true, required: true},
     password: {type: String, required: true},
+    status: {type: String},
+    name: {type: String},
     role: {type: String, ref: 'Role'}
 });
 
