5ef3969090c19521bca711e17a234683fe508a10
vovoka <vovoka.path@gmail.com>
2022-08-30 18:31:25 +0300

    feat: render list i n photographerList

--- crm/src/components/photographerList/photographerList.js
+++ crm/src/components/photographerList/photographerList.js
@@ -4,35 +4,45 @@ import cabViews from '../../data/cabViews.json' assert { type: "json" };
 
 const styles = {
     container: 'photographer-list-container',
-    photogrpherContainer: 'photographer-container',
+    photographerContainer: 'photographer-container',
+    photographerKey: 'photographer-key',
     button: 'photographer-button',
 }
 
 class PhotographerList {
     constructor() {
         this.container = createCustomElement('div', styles.container);
-        this.photogrpherContainer = createCustomElement('div', styles.photogrpherContainer);
         this.statusButtons = [];
     }
 
     create(props) {
         const { users } = props;
-        console.log('# props = ', props);
+        // console.log('# props = ', props);
         const photographers = users.filter((user) => user.role === 'photographer');
 
         photographers.forEach((photographer) => {
-            this.photographer = document.createElement('h3');
+            // this.photographer = document.createElement('h3');
+            // console.log('# photographer = ', photographer, Object.entries(photographer));
+            const photographerContainer = createCustomElement('div', styles.photographerContainer);
 
-            this.photographer.innerText = photographer.username;
-            this.photogrpherContainer.append(this.photographer);
+            for (let [key, value] of Object.entries(photographer)) {
+                if (!['password', 'role', 'username', '__v', '_id'].includes(key)) {
+                    const photographerKey = createCustomElement('div', `${styles.photographerKey} photographer-${key}`);
+                    photographerKey.innerText = photographer[key];
 
-            setAttributesElement(this.photogrpherContainer, {
+                    photographerContainer.append(photographerKey);
+                }
+            }
+
+            this.container.append(photographerContainer);
+
+            setAttributesElement(photographerContainer, {
                 'name': photographer.name,
                 'status': photographer.status,
                 'id': photographer._id,
             })
             
-            this.renderStatusButtons(props);
+            this.renderStatusButtons(photographerContainer, props);
 
             // this.StatusButton = new StatusButton() 
             // const statusButtonTexts = cabViews[role][roleStatus].statusButtonText;
@@ -44,12 +54,11 @@ class PhotographerList {
             // this.photogrpherContainer.append(this.button);
         });
         
-        this.container.append(this.photogrpherContainer);
 
         return this.container;
     }
 
-    renderStatusButtons(props) {
+    renderStatusButtons(photographerContainer, props) {
         const { role, roleStatus, orderStatuses, order, orderButtonListener, statusButtonListener } = props;
         const lang = 'ru';
         const statusButtonTexts = cabViews[role][roleStatus].statusButtonText;
@@ -69,7 +78,7 @@ class PhotographerList {
                 // Вешаем обработчик
                 this.statusButton.addEventListener('click', statusButtonListener()); // TODO listener
                 
-                this.photogrpherContainer.append(this.statusButton);
+                photographerContainer.append(this.statusButton);
                 // this.changedElement.append(this.statusButton);
             }
         }
