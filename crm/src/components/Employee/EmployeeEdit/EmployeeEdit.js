e68c5aff2745ad4ef2d8d0b899e61e35f2075a7e
Pavel Lebedev <lebedpavel.dev@gmail.com>
2022-09-04 11:35:11 +0300

    feat: create edit user component

--- crm/src/components/Employee/EmployeeEdit/EmployeeEdit.js
+++ crm/src/components/Employee/EmployeeEdit/EmployeeEdit.js
@@ -1,5 +1,8 @@
 import Container from "../../Container/Container.js";
 import Header from "../../Header/Header.js";
+import Label from '../../Label/Label.js';
+import Input from '../../Input/Input.js';
+import Button from "../../Button/Button.js";
 
 class EmployeeEdit {
     constructor(cabContainer, props) {
@@ -8,15 +11,43 @@ class EmployeeEdit {
         this.create = this.create;
     }
     create(user) {
-        this.cabContainer.innerHTML = 'View EmployeeEdit. USER id = ' + user._id; // заменить на: this.cabContainer.innerHTML = '';
+        this.cabContainer.innerHTML = '';
+        // this.cabContainer.innerHTML = 'View EmployeeEdit. USER id = ' + user._id; // заменить на: this.cabContainer.innerHTML = '';
         // console.log('# create(user) = ', user);
         const { btnUpdateUserListener } = this.props; // обновит данные пользователя по id кнопки = 630e16043f4dd1fd2ac94429
-        const userItem = Container.create('user-item__container');
-        for(let prop in user){
-            const item = Container.create(`user-item__${prop}`, user[prop]);
-            userItem.append(item);
+
+        const userEdit = Container.create('user-edit__container');
+        const userEditBtn = Button.create('user-edit__btn', 'Сохранить изменения', 'userEditBtn', this.createUpdateData(user));
+        const labels = ['Имя пользователя', 'Пароль', 'Роль', 'E-mail', 'Полное имя'];
+        const inputs = ['username', 'password', 'role', 'email', 'name'];
+
+        labels.forEach((item, i) => {
+            const label = Label.create(`userEdit-${inputs[i]}`, item);
+            const input = Input.create(`user-edit__item`, 'text', user[inputs[i]], `userEdit-${inputs[i]}`, user[inputs[i]]);
+            label.append(input);
+
+            userEdit.append(label);
+        });
+
+        userEdit.append(userEditBtn);
+
+        this.cabContainer.append(userEdit);
+    }
+
+    createUpdateData(user) {
+        return () => {
+            const userData =  {
+                _id: user._id,
+            };
+            const inputs = ['username', 'password', 'role', 'email', 'name'];
+            inputs.forEach((el) => {
+                const curInput = document.querySelector(`#userEdit-${el}`);
+                userData[el] = curInput.value;
+            });
+    
+            console.log(userData);
         }
-        return userItem;
+
     }
 }
 
